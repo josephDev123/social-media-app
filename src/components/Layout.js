d0f9a737c259923c7291ffa197666fd490632d49
uzuegbu joseph <josephuzuegbu55@gmail.com>
2022-02-07 22:13:29 +0100

    modified:   public/index.html         modified:   src/components/Layout.js         modified:   src/components/pages/Home.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Sidebar from '../components/pages/Sidebar';
-import Feed from './Feed';
+// import Feed from './Feed';
 import TwitterEmbed from './TwitterEmbed';
+import Home from './pages/Home';
 
 export default function Layout() {
   return(
     <div className='container'>
       <div className='row'>
-        <div className='col-5 col-sm-10 col-md-4'>
+        <div className='col-lg-4 col-sm-12 col-md-4'>
           <Sidebar/>
         </div>
 
-        <div className='col-5 col-sm-10 col-md-4'>
-          <Feed/>
+        <div className='col-lg-4 col-sm-12 col-md-4'>
+          <Home/>
         </div>
 
-        <div className='col-5 col-sm-10 col-md-4'>
+        <div className='col-lg-4 col-sm-12 col-md-4'>
           <TwitterEmbed/>
         </div>
       </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import '../css/home_page.css';
 
 export default function Home() {
   return (
-    <div>
-        <h3>Home</h3>
+    <div className='home_container'>
+        <h6 className='pb-4'>Home</h6>
+
+        <div class="input-group form-group-sm">
+          <span class="input-group-text"><img src='asset/avatar/avatar.jpg' className='img-fluid' width='30px' height='30px'></img></span>
+          <textarea class="form-control tweet_box" width='20px' height='20px' placeholder="What is Happening?"></textarea>
+        </div>
+    <hr/>
+        <div className='d-flex justify-content-between mt-4'>
+          <div class="image-upload">
+            <label for="file-input">
+              <i class="fas fa-file-upload img"></i>
+            </label>
+
+            <input id="file-input" type="file" />
+          </div>
+            <div className='tweet_btn_wrapper'>
+              <button className='tweet_btn'>Tweet</button>
+            </div>
+        </div>
+      <hr/>
+        {/* feed component */}
+
+
     </div>
   );
 }
