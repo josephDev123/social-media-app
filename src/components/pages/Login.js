57b1401b7955c0a6ff08a55d474c8658a5bdbecd
uzuegbu joseph <josephuzuegbu55@gmail.com>
2022-02-09 16:16:39 +0100

    modified:   src/components/pages/Login.js         modified:   src/components/pages/Register.js, created public/asset/login_reg_bg_img/         src/components/css/login_reg.css

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/login_reg.css';
 
 export default function Login() {
+    const myStyle={
+        backgroundImage:`url(${process.env.PUBLIC_URL+ "/asset/login_reg_bg_img/login_img_bg1.png"})`,
+       height:"100vh",
+       width:"100vh",
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        };
+
+
+
   return( 
-    <div className='container d-flex flex-column justify-content-center align-items-center'>
-       
-        <form>
+    <div className='container d-flex flex-column justify-content-center align-items-center login_wrapper' style={myStyle}>
+        <form  style={{ marginTop:'30vh' }}>
             <div className="mb-3">
             <h4>Login</h4>
                 <label htmlFor="loginInputEmail1" className="form-label">Email address</label>
@@ -15,9 +26,11 @@ export default function Login() {
                 <label htmlFor="loginInputPassword1" className="form-label">Password</label>
                 <input type="password" className="form-control" id="loginInputPassword1"/>
             </div>
-            <button type="submit" className="btn btn-primary">Login</button>
+            <div class="input-group mb-3">
+                <button type="submit" className="btn btn-primary me-2">Login</button>
+                <Link className='btn btn_bg-littleStyle' to='/register'>Register</Link>
+            </div>
         </form>
-        
     </div>
 );
 }
--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/login_reg.css';
 
 export default function Register() {
+
+    const myStyle={
+        backgroundImage:`url(${process.env.PUBLIC_URL+ "/asset/login_reg_bg_img/register_bg_img.png"})`,
+               height:"100vh",
+               width:"100vh",
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                };
   return( 
-        <div className='container d-flex flex-column justify-content-center align-items-center'>
-            <form>
+      <div style={myStyle}>
+        <div className=' d-flex flex-column justify-content-center align-items-center register_wrapper' >
+            <form className='' style={{ marginTop:'30vh' }}>
                 <div className="mb-3">
                 <h4>Register</h4>
                     <label htmlFor="registerInputEmail1" className="form-label">Email address</label>
@@ -14,9 +25,14 @@ export default function Register() {
                     <label htmlFor="registerInputPassword1" className="form-label">Password</label>
                     <input type="password" className="form-control" id="registerInputPassword1"/>
                 </div>
-                <button type="submit" className="btn btn-primary">Register</button>
+               
+                <div class="input-group mb-3">
+                     <button type="submit" className="btn btn-primary me-2">Register</button>
+                    <Link className='btn btn_bg-littleStyle' to='/login'>Login</Link>
+                </div>
             </form>
             
+        </div>
         </div>
   );
 }
